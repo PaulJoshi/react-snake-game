b53666fc0f276c85949a5dd01e70eaba13f28c46
Paul Joshi <49583322+PaulJoshi@users.noreply.github.com>
2020-07-24 15:10:03 +0530

    Updates

---
+++ precache-manifest.9b30141f60ee19d54e412a59b09badd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b269ffc9459931e6cf4b088fb6a30f",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "9ceebb94746c448f5d03",
+    "url": "/snake-game/static/css/main.231ad360.chunk.css"
+  },
+  {
+    "revision": "a4b87b5d0751dd1c78ce",
+    "url": "/snake-game/static/js/2.13263a00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game/static/js/2.13263a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ceebb94746c448f5d03",
+    "url": "/snake-game/static/js/main.84425f06.chunk.js"
+  },
+  {
+    "revision": "ae79cfb0705d97865ae5",
+    "url": "/snake-game/static/js/runtime-main.9f7fa830.js"
+  }
+]);
